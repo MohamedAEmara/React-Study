9431be900e0fe4501c395b699a6bbff701f36f53
Mohamed Emara <medoemara104@gmail.com>
2024-04-15 00:07:22 +0200

    Use html inside .ejs files

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -1,44 +1,40 @@
-import React from 'react';
+// import React from 'react';
 import { createRoot } from 'react-dom';
-const Pet = (props) => {
-  return React.createElement(
-    'div',
-    {},
-    [
-      // An array of components to return
-      React.createElement('h1', {}, props.name),
-      React.createElement('h2', {}, props.animal),
-      React.createElement('h3', {}, props.breed),
-    ]
-    // NOTE: If we have a single element, no need to include "[Square Brackets]"
-  );
-};
+import Pet from './Pet';
 
 const App = () => {
-  return React.createElement(
-    'div', // Element type
-    { id: 'my-id' }, // Attributes of the element like id, class,
-    [
-      React.createElement('h1', {}, 'Adopt me...'),
-      React.createElement(Pet, {
-        name: 'Luna',
-        animal: 'Dog',
-        breed: 'Havanese',
-      }),
-      React.createElement(Pet, {
-        name: 'Pepper',
-        animal: 'Bird',
-        breed: 'Cockatiel',
-      }),
-      React.createElement(Pet, {
-        name: 'Doink',
-        animal: 'Cat',
-        breed: 'Mixed',
-      }),
-    ]
-  );
+  <div id="my-id">
+    <h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" />
+    </h1>
+    
+  </div>
+  // return React.createElement(
+  //   'div', // Element type
+  //   { id: 'my-id' }, // Attributes of the element like id, class,
+  //   [
+  //     React.createElement('h1', {}, 'Adopt me...'),
+  //     React.createElement(Pet, {
+  //       name: 'Luna',
+  //       animal: 'Dog',
+  //       breed: 'Havanese',
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: 'Pepper',
+  //       animal: 'Bird',
+  //       breed: 'Cockatiel',
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: 'Doink',
+  //       animal: 'Cat',
+  //       breed: 'Mixed',
+  //     }),
+  //   ]
+  // );
 };
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
---
+++ adopt-me/src/Pet.jsx
@@ -0,0 +1,26 @@
+
+// const Pet = (props) => {
+//     return React.createElement(
+//       'div',
+//       {},
+//       [
+//         React.createElement('h1', {}, props.name),
+//         React.createElement('h2', {}, props.animal),
+//         React.createElement('h3', {}, props.breed),
+//       ]
+//     );
+// };
+
+// We'll use HTML directly...
+const Pet = (props) => {
+    return (
+        <div>
+            <h1>{props.name}</h1>
+            <h2>{props.animal}</h2>
+            <h3>{props.breed}</h3>
+        </div>
+    )
+}
+// It's easier to read the second one..
+
+export default Pet;
