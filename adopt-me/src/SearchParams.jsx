e9ff3b3f4985ebcc0f96252d471f1afab27b8fc2
Mohamed Emara <medoemara104@gmail.com>
2024-04-15 20:49:27 +0200

    Use useEffect to request from API

--- adopt-me/src/SearchParams.jsx
+++ adopt-me/src/SearchParams.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
+import Pet from './Pet';
 const ANIMALS = ['bird', 'cat', 'dog', 'rabbit', 'reptile'];
 
 let cnt = 0;
@@ -8,15 +8,44 @@ const SearchParams = () => {
   const [location, setLocation] = useState('');
   const [animal, setAnimal] = useState('');
   const [breed, setBreed] = useState('');
+  const [pets, setPets] = useState([]); // Empty array that we'll retrieve later from the API
   const breeds = [];
 
+  // NOTE: by default, "useEffect" renders everytime a change happens
+  //    but we don't it to act this way.
+  // We can put a dependency that triggers calling this function
+  useEffect(
+    () => {
+      requestPets();
+    },
+    [
+      /*requestPets*/
+    ]
+  ); // For example here. in the commented part, I told it to rerun if a change happens to (requestPets)
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&city=${location}`
+    );
+
+    const json = await res.json();
+    console.log(json);
+    console.log('------------------------');
+    console.log(res);
+    setPets(json.pets);
+  }
   return (
     // We use parentheses to tell JS that we will return in the next line.
     // NOTE: "class" is a reserverd keyword for JS so we used (className) instead.
 
     <div className="search-params">
       <h1> {cnt} </h1>
-      <form action="">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets(); // Now fetching from the API won't happen until we click the submit button.
+        }}
+      >
         <label htmlFor="location">
           Location:
           <input
@@ -49,7 +78,7 @@ const SearchParams = () => {
             id="breed"
             disabled={breeds.length === 0}
             value={breed}
-            onChange={(e) => setbreed(e.target.value)}
+            onChange={(e) => setBreed(e.target.value)}
           >
             <option />
             {/* This map returns an array of <option> with key and textContent equal the value of the element  */}
@@ -60,6 +89,14 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
+      {pets.map((pet) => {
+        <Pet
+          name={pet.name}
+          animal={pet.animal}
+          breed={pet.breed}
+          key={pet.id}
+        />;
+      })}
     </div>
   );
 };
