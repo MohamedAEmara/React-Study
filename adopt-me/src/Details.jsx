649058fed39006082aa9da7e2d687c7854c0d32a
Mohamed Emara <medoemara104@gmail.com>
2024-04-17 15:30:57 +0200

    Use useParams() hook

--- adopt-me/src/Details.jsx
+++ adopt-me/src/Details.jsx
@@ -1,5 +1,15 @@
+import { useParams } from 'react-router-dom';
+
 const Details = () => {
-  return <h2>hi!</h2>;
+  const { id } = useParams();
+  // NOTE: You have to use <BrowserRouter> element in App.jsx
+  //      to be able to use (useParams) hook
+  return (
+    <h2>
+      hi!
+      <h3>{id}</h3>
+    </h2>
+  );
 };
 
 export default Details;
