defbb72f385519bfd76e025b42cb59ea5c588896
Mohamed Emara <medoemara104@gmail.com>
2024-04-25 17:05:24 +0200

    Implement details page & cash results

--- adopt-me/src/Details.jsx
+++ adopt-me/src/Details.jsx
@@ -1,14 +1,42 @@
 import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import fetchPet from './fetchPet';
 
 const Details = () => {
   const { id } = useParams();
   // NOTE: You have to use <BrowserRouter> element in App.jsx
   //      to be able to use (useParams) hook
+ 
+  const results = useQuery(["details", id], fetchPet);
+  // Search for this id in your cash, if you don't find it run this >> fetchPet <<
+
+ console.log(results.isError);
+ if(results.isError) {
+  return <h2>Oh, noooooooooooooo</h2>
+ }
+  // By default this loading symbol will run
+  // and as soon as the fetch completed, the page will be rerendered and loading symbol will disapear.
+  if(results.isLoading) {
+    return (
+      <div className='loading-pane'>
+        <h2 className='loader'>🌀</h2>
+      </div>
+    );
+  }
+
+  const pet = results.data.pets[0];
+  
   return (
-    <h2>
-      hi!
-      <h3>{id}</h3>
-    </h2>
+    <div className='details'>
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>
+          {pet.animal} - {pet.bread} - {pet.city}, {pet.state}
+          <button>Adopt {pet.name}</button>
+          <p>{pet.description}</p>
+        </h2>
+      </div>
+    </div>
   );
 };
 
